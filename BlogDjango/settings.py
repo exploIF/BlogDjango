9f5c8e9047c55db424dc4e8f403b66d372d0b368
exploIF <74370735+exploIF@users.noreply.github.com>
2021-03-16 21:15:21 +0100

    Update settings.py

--- BlogDjango/settings.py
+++ BlogDjango/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'p9fma40@tje!rkr77yafo+4ls1q@f5z8638!dv-d)+cq9zim19'
+SECRET_KEY = '##########'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
